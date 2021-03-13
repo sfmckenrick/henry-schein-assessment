690650a51aa6dda2dce46fbfa1fbf5d54975f389
Scott McKenrick <sbm5967@arl.psu.edu>
2021-03-12 19:59:54 -0500

    Creates the basic packaging structure, main class and basic spring properties for the database connection.

---
+++ src/main/java/com/sfmckenrick/assessment/Application.java
@@ -0,0 +1,14 @@
+package com.sfmckenrick.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Scott McKenrick <sbm5967@arl.psu.edu>
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
