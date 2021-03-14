fc51d9101b5274e5e71bfc77af189ea365239294
Scott McKenrick <sbm5967@arl.psu.edu>
2021-03-13 21:05:29 -0500

    Creates Repository interface for accessing the ClubMembership data. Implements Unit Tests for new Service Methods. Fixes errors discovered with the Service layer while developing the Unit tests.

--- src/main/java/com/sfmckenrick/assessment/personManagement/PersonManagementService.java
+++ src/main/java/com/sfmckenrick/assessment/personManagement/PersonManagementService.java
@@ -1,11 +1,14 @@
 package com.sfmckenrick.assessment.personManagement;
 
+import com.sfmckenrick.assessment.personManagement.exception.AddressNotFoundException;
+import com.sfmckenrick.assessment.personManagement.exception.ClubNotFoundException;
+import com.sfmckenrick.assessment.personManagement.exception.PersonNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Contains all business logic for persisting and retrieving all entities related to a Person.
@@ -21,9 +24,14 @@ public class PersonManagementService {
     private final AddressRepository addressRepository;
 
     /**
+     * The configured repository for the ClubMembership mapping entity.
+     */
+    private final ClubMembershipRepository clubMembershipRepository;
+
+    /**
      * The configured repository for the Club entity.
      */
-//    private final ClubRepository clubRepository;
+    private final ClubRepository clubRepository;
 
     /**
      * The configured repository for the Person entity.
@@ -33,25 +41,28 @@ public class PersonManagementService {
     /**
      * Constructor.
      * @param addressRepository - The repository to use for interacting with Address entities.
-//     * @param clubRepository - The repository to use for interacting with Club entities.
+     * @param clubMembershipRepository - The repository to use for interacting with Club/Person mappings.
+     * @param clubRepository - The repository to use for interacting with Club entities.
      * @param personRepository - The repository to use for interacting with Person entities.
      */
     @Autowired
     public PersonManagementService(AddressRepository addressRepository,
-                                   /*ClubRepository clubRepository,*/
+                                   ClubMembershipRepository clubMembershipRepository,
+                                   ClubRepository clubRepository,
                                    PersonRepository personRepository) {
         this.addressRepository = addressRepository;
-//        this.clubRepository = clubRepository;
+        this.clubMembershipRepository = clubMembershipRepository;
+        this.clubRepository = clubRepository;
         this.personRepository = personRepository;
     }
 
     /**
      * Gets an Optional-wrapped Person object by its ID
      * @param id - The ID of the person.
-     * @return An Optional Person with matching ID.
+     * @return A Person with matching ID.
      */
-    public Optional<Person> getPersonById(Long id) {
-        return personRepository.findById(id);
+    public Person getPersonById(Long id) {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
     }
 
     /**
@@ -69,7 +80,7 @@ public class PersonManagementService {
      * @param id - The ID of the Person to delete.
      */
     @Transactional
-    public void deletePersonById(Long id) {
+    public void deletePersonById(long id) {
         personRepository.deleteById(id);
     }
 
@@ -88,17 +99,17 @@ public class PersonManagementService {
      * @param id - The ID of the Person to find all Addresses.
      * @return The List of Address objects.
      */
-    public List<Address> getAddressByPersonId(Long id) {
+    public List<Address> getAddressByPersonId(long id) {
         return addressRepository.findByPersonId(id);
     }
 
     /**
      * Gets an Optional-wrapped Address object by its ID.
      * @param id - The ID of the Address.
-     * @return An Optional Address with matching ID.
+     * @return An Address with matching ID.
      */
-    public Optional<Address> getAddressById(Long id) {
-        return addressRepository.findById(id);
+    public Address getAddressById(long id) {
+        return addressRepository.findById(id).orElseThrow(() -> new AddressNotFoundException(id));
     }
 
     /**
@@ -106,7 +117,7 @@ public class PersonManagementService {
      * @param id - The ID of the Person whose Addresses are to be deleted.
      */
     @Transactional
-    public void deleteAllAddressForPersonById(Long id) {
+    public void deleteAllAddressForPersonById(long id) {
         addressRepository.deleteAddressByPersonId(id);
     }
 
@@ -114,8 +125,120 @@ public class PersonManagementService {
      * Deletes an Address by its ID.
      * @param id - The ID of the Address to delete.
      */
-    public void deleteAddressById(Long id) {
+    @Transactional
+    public void deleteAddressById(long id) {
         addressRepository.deleteById(id);
     }
 
+    /**
+     * Saves (Inserts or Updates) a Club.
+     * @param club - The Club object to save.
+     * @return The saved Club.
+     */
+    @Transactional
+    public Club saveClub(Club club) {
+        return clubRepository.save(club);
+    }
+
+    /**
+     * Deletes a Club by its name identifier.
+     * @param name The name of the Club to delete.
+     */
+    @Transactional
+    public void deleteClubByName(String name) {
+        clubRepository.deleteById(name);
+    }
+
+    /**
+     * Gets a Club by its name identifier.
+     * @param name The name of the club to get.
+     * @return A Club object with a matching name.
+     */
+    public Club getClubByName(String name) {
+        return clubRepository.findById(name).orElseThrow(() ->new ClubNotFoundException(name));
+    }
+
+    /**
+     * Creates a new relationship between a Person and a Club.
+     * @param personId - The person to add to a Club.
+     * @param clubName - The Club where the person is to be added.
+     * @return The result ClubMembership.
+     */
+    public ClubMembership addClubMembership(long personId, String clubName) {
+        Person person = getPersonById(personId);
+        Club club = getClubByName(clubName);
+        ClubMembership clubMembership = new ClubMembership(person, club);
+        return clubMembershipRepository.save(clubMembership);
+    }
+
+    /**
+     * Deletes a specific relationship between a Person and a Club.
+     * @param personId - The ID of the Person who belongs to a club.
+     * @param clubName - The Name of the Club where the person is a member.
+     */
+    @Transactional
+    public void deleteClubMembership(long personId, String clubName) {
+        clubMembershipRepository.deleteByIdPersonIdAndIdClubName(personId, clubName);
+    }
+
+    /**
+     * Checks to see if a Person has membership with a Club.
+     * @param personId - The ID of the Person to check.
+     * @param clubName - The name of the Club to check.
+     * @return True if member, false if no relation.
+     */
+    public boolean isPersonClubMember(long personId, String clubName) {
+        return clubMembershipRepository.existsByIdPersonIdAndIdClubName(personId, clubName);
+    }
+
+    /**
+     * Deletes all Club memberships involving a single Person.
+     * @param id - The ID of the Person to remove.
+     */
+    @Transactional
+    public void deleteClubMembershipByPerson(long id) {
+        clubMembershipRepository.deleteByIdPersonId(id);
+    }
+
+
+    /**
+     * Deletes all Club memberships involving a single Club.
+     * @param name - The name of the Club to remove.
+     */
+    @Transactional
+    public void deleteClubMembershipByClub(String name) {
+        clubMembershipRepository.deleteByIdClubName(name);
+    }
+
+    /**
+     * Gets all Clubs where a Person is a member.
+     * @param id - The ID of the Person to Check.
+     * @return A list of Clubs where the supplied Person is a member.
+     */
+    public List<Club> getClubMembershipForPerson(Long id) {
+        return clubMembershipRepository.findByIdPersonId(id)
+                .stream()
+                .map(ClubMembership::getClub)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Gets all Persons that belong to a specified Club.
+     * @param name - The name of the Club to check.
+     * @return A list of Persons where they belong to a supplied Club.
+     */
+    public List<Person> getPersonMembersForClub(String name) {
+        return clubMembershipRepository.findByIdClubName(name)
+                .stream()
+                .map(ClubMembership::getPerson)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Clears the entire datastore.
+     */
+    protected void clear() {
+        personRepository.deleteAll();
+        clubRepository.deleteAll();
+    }
 }
--- src/test/java/com/sfmckenrick/assessment/personManagement/PersonManagementServiceTest.java
+++ src/test/java/com/sfmckenrick/assessment/personManagement/PersonManagementServiceTest.java
@@ -1,23 +1,32 @@
 package com.sfmckenrick.assessment.personManagement;
 
+import com.sfmckenrick.assessment.personManagement.exception.AddressNotFoundException;
+import com.sfmckenrick.assessment.personManagement.exception.ClubNotFoundException;
+import com.sfmckenrick.assessment.personManagement.exception.PersonNotFoundException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Test Suite to test the PersonManagementService.
  * @author Scott McKenrick <sbm5967@arl.psu.edu>
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class PersonManagementServiceTest {
 
     @Autowired
     private PersonManagementService service;
+
+    @BeforeEach
+    public void clean() {
+        service.clear();
+    }
 
     @Test
     public void testAddPerson() {
@@ -43,9 +52,9 @@ public class PersonManagementServiceTest {
     public void testDeletePersonNoAddress() {
         Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
         service.savePerson(p);
-        Assertions.assertTrue(service.getPersonById(p.getId()).isPresent());
+        Assertions.assertNotNull(service.getPersonById(p.getId()));
         service.deletePersonById(p.getId());
-        Assertions.assertTrue(service.getPersonById(p.getId()).isEmpty());
+        Assertions.assertThrows(PersonNotFoundException.class, () -> service.getPersonById(p.getId()));
     }
 
     @Test
@@ -58,8 +67,8 @@ public class PersonManagementServiceTest {
 
         service.deletePersonById(p.getId());
 
-        Assertions.assertTrue(service.getPersonById(p.getId()).isEmpty());
-        Assertions.assertTrue(service.getAddressById(a1.getId()).isEmpty());
+        Assertions.assertThrows(PersonNotFoundException.class, () -> service.getPersonById(p.getId()));
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a1.getId()));
     }
 
     @Test
@@ -73,9 +82,9 @@ public class PersonManagementServiceTest {
         service.saveAddress(a2);
         service.deletePersonById(p.getId());
 
-        Assertions.assertTrue(service.getPersonById(p.getId()).isEmpty());
-        Assertions.assertTrue(service.getAddressById(a1.getId()).isEmpty());
-        Assertions.assertTrue(service.getAddressById(a2.getId()).isEmpty());
+        Assertions.assertThrows(PersonNotFoundException.class, () -> service.getPersonById(p.getId()));
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a1.getId()));
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a2.getId()));
 
     }
 
@@ -84,8 +93,8 @@ public class PersonManagementServiceTest {
         Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
         service.savePerson(p);
 
-        Optional<Person> maybePerson = service.getPersonById(p.getId());
-        Assertions.assertEquals(p, maybePerson.get());
+        Person result = service.getPersonById(p.getId());
+        Assertions.assertEquals(p, result);
     }
 
     @Test
@@ -110,8 +119,8 @@ public class PersonManagementServiceTest {
         service.saveAddress(a2);
 
         service.deleteAllAddressForPersonById(p.getId());
-        Assertions.assertTrue(service.getAddressById(a1.getId()).isEmpty());
-        Assertions.assertTrue(service.getAddressById(a2.getId()).isEmpty());
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a1.getId()));
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a2.getId()));
 
     }
 
@@ -124,8 +133,8 @@ public class PersonManagementServiceTest {
         service.saveAddress(a1);
 
         service.deleteAddressById(a1.getId());
-        Assertions.assertTrue(service.getAddressById(a1.getId()).isEmpty());
-        Assertions.assertTrue(service.getPersonById(p.getId()).isPresent());
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a1.getId()));
+        Assertions.assertNotNull(service.getPersonById(p.getId()));
 
     }
 
@@ -155,8 +164,8 @@ public class PersonManagementServiceTest {
         Address a2 = new Address("456 Blvd", "Townsville", State.OKLAHOMA, "98765", p);
         service.saveAddress(a2);
 
-        Assertions.assertTrue(service.getAddressById(a1.getId()).isPresent());
-        Assertions.assertTrue(service.getAddressById(a2.getId()).isPresent());
+        Assertions.assertNotNull(service.getAddressById(a1.getId()));
+        Assertions.assertNotNull(service.getAddressById(a2.getId()));
 
 
     }
@@ -169,8 +178,8 @@ public class PersonManagementServiceTest {
         Address a1 = new Address("123 Street", "Cityville", State.ALABAMA, "12345", p);
         service.saveAddress(a1);
 
-        Optional<Address> maybeAddress = service.getAddressById(a1.getId());
-        Assertions.assertEquals(a1, maybeAddress.get());
+        Address result = service.getAddressById(a1.getId());
+        Assertions.assertEquals(a1, result);
 
     }
 
@@ -191,4 +200,218 @@ public class PersonManagementServiceTest {
         Assertions.assertTrue(addressList.contains(a1));
         Assertions.assertTrue(addressList.contains(a2));
     }
+
+    @Test
+    public void testAddClub() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+    }
+
+    @Test
+    public void testGetClubByName() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+        Club result = service.getClubByName(club.getName());
+        Assertions.assertEquals(club, result);
+    }
+
+    @Test
+    public void testUpdateClub() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+        String name = club.getName();
+
+        club.setDescription("new");
+        service.saveClub(club);
+        Assertions.assertEquals(name, club.getName());
+        Assertions.assertEquals("new", club.getDescription());
+    }
+
+    @Test
+    public void testDeleteClub() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+        service.deleteClubByName(club.getName());
+        Assertions.assertThrows(ClubNotFoundException.class, () -> service.getClubByName(club.getName()));
+    }
+
+    @Test
+    public void testAddClubMembership() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        Assertions.assertEquals(club, membership.getClub());
+        Assertions.assertEquals(p, membership.getPerson());
+        Assertions.assertTrue(service.isPersonClubMember(p.getId(), club.getName()));
+    }
+
+    @Test
+    public void testAddDuplicateClubMembership() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        Assertions.assertNotNull(membership.getClub());
+        Assertions.assertNotNull(membership.getPerson());
+
+        Assertions.assertThrows(DataIntegrityViolationException.class,
+                () -> service.addClubMembership(p.getId(), club.getName()));
+    }
+
+    @Test
+    public void testDeleteClubMembership() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        Assertions.assertEquals(club, membership.getClub());
+        Assertions.assertEquals(p, membership.getPerson());
+
+        service.deleteClubMembership(p.getId(), club.getName());
+        Assertions.assertFalse(service.isPersonClubMember(p.getId(), club.getName()));
+        Assertions.assertNotNull(service.getPersonById(p.getId()));
+        Assertions.assertNotNull(service.getClubByName(club.getName()));
+    }
+
+    @Test
+    public void testDeleteClubMembershipByPerson() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Club club2 = new Club("Test Club 2", null);
+        service.saveClub(club2);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        ClubMembership membership2 = service.addClubMembership(p.getId(), club2.getName());
+
+        Assertions.assertTrue(service.isPersonClubMember(p.getId(), club.getName()));
+        Assertions.assertTrue(service.isPersonClubMember(p.getId(), club2.getName()));
+
+        service.deleteClubMembershipByPerson(p.getId());
+        Assertions.assertFalse(service.isPersonClubMember(p.getId(), club.getName()));
+        Assertions.assertFalse(service.isPersonClubMember(p.getId(), club2.getName()));
+    }
+
+    @Test
+    public void testDeleteClubMembershipByClub() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        Person p2 = new Person("John", "William", "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p2);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        ClubMembership membership2 = service.addClubMembership(p2.getId(), club.getName());
+
+        Assertions.assertTrue(service.isPersonClubMember(p.getId(), club.getName()));
+        Assertions.assertTrue(service.isPersonClubMember(p2.getId(), club.getName()));
+
+        service.deleteClubMembershipByClub(club.getName());
+        Assertions.assertFalse(service.isPersonClubMember(p.getId(), club.getName()));
+        Assertions.assertFalse(service.isPersonClubMember(p2.getId(), club.getName()));
+    }
+
+    @Test
+    public void testGetClubMembershipByPerson() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Club club2 = new Club("Test Club 2", null);
+        service.saveClub(club2);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        ClubMembership membership2 = service.addClubMembership(p.getId(), club2.getName());
+
+        Assertions.assertTrue(service.isPersonClubMember(p.getId(), club.getName()));
+        Assertions.assertTrue(service.isPersonClubMember(p.getId(), club2.getName()));
+
+        List<Club> clubs = service.getClubMembershipForPerson(p.getId());
+        Assertions.assertEquals(2, clubs.size());
+        Assertions.assertTrue(clubs.contains(club));
+        Assertions.assertTrue(clubs.contains(club2));
+    }
+
+    @Test
+    public void testGetClubMembershipByClub() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        Person p2 = new Person("John", "William", "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p2);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+        ClubMembership membership2 = service.addClubMembership(p2.getId(), club.getName());
+
+        List<Person> persons = service.getPersonMembersForClub(club.getName());
+
+        Assertions.assertEquals(2, persons.size());
+        Assertions.assertTrue(persons.contains(p));
+        Assertions.assertTrue(persons.contains(p2));
+    }
+
+    @Test
+    public void testCascadeDeletePerson() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        Address a1 = new Address("123 Street", "Cityville", State.ALABAMA, "12345", p);
+        service.saveAddress(a1);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+
+        service.deletePersonById(p.getId());
+
+        Assertions.assertNotNull(service.getClubByName(club.getName()));
+        Assertions.assertThrows(AddressNotFoundException.class, () -> service.getAddressById(a1.getId()));
+        Assertions.assertThrows(PersonNotFoundException.class, () -> service.getPersonById(p.getId()));
+        Assertions.assertFalse(service.isPersonClubMember(p.getId(), club.getName()));
+    }
+
+    @Test
+    public void testCascadeDeleteClub() {
+        Club club = new Club("Test Club", null);
+        service.saveClub(club);
+
+        Person p = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
+        service.savePerson(p);
+
+        Address a1 = new Address("123 Street", "Cityville", State.ALABAMA, "12345", p);
+        service.saveAddress(a1);
+
+        ClubMembership membership = service.addClubMembership(p.getId(), club.getName());
+
+        service.deleteClubByName(club.getName());
+
+        Assertions.assertThrows(ClubNotFoundException.class, () -> service.getClubByName(club.getName()));
+        Assertions.assertNotNull(service.getAddressById(a1.getId()));
+        Assertions.assertNotNull(service.getPersonById(p.getId()));
+        Assertions.assertFalse(service.isPersonClubMember(p.getId(), club.getName()));
+    }
+
+
 }
