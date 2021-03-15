12cf412e640d45e135474c778af4d768d19fa50f
Scott McKenrick <sbm5967@arl.psu.edu>
2021-03-15 03:27:46 -0400

    Creates JWT utility for creating JWT Tokens.

---
+++ src/main/java/com/sfmckenrick/assessment/authentication/JwtUtil.java
@@ -0,0 +1,88 @@
+package com.sfmckenrick.assessment.authentication;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Utility Class for creating and parsing JWT Tokens.
+ * @author Scott McKenrick <sbm5967@arl.psu.edu>
+ */
+@Component
+public class JwtUtil {
+
+    /**
+     * The key to use for adding the Authorities to the JWT Claims.
+     */
+    private final String AUTHORITIES_KEY = "authorities";
+
+    /**
+     * The secret to use when encrypting the token.
+     */
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.valid.millis}")
+    private Long tokenExpiryMillis;
+
+    /**
+     * Attempts to parse a JWT. Returns an Optional-wrapped User representing the token's owner.
+     *
+     * @param token the JWT token to parse
+     * @return The username of the requesting user.
+     */
+    public Optional<User> parseToken(String token) {
+        try {
+            Claims body = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String username = body.getSubject();
+            String password = "";
+            Collection<GrantedAuthority> authorities= Arrays.stream(body.get(AUTHORITIES_KEY).toString().split(","))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+            return Optional.of(new User(username, password, authorities));
+
+        } catch (JwtException e) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Generates a simple JWT with a Username subject and an Expiry time.
+     *
+     * @param user - The User for which the token will be generated
+     * @return the JWT token
+     */
+    public String generateToken(UserDetails user) {
+        Claims claims = Jwts.claims().setSubject(user.getUsername());
+        claims.put(AUTHORITIES_KEY, user.getAuthorities());
+
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime expirationDateTime = now.plus(this.tokenExpiryMillis, ChronoUnit.MILLIS);
+
+        Date issueDate = Date.from(now.toInstant());
+        Date expirationDate = Date.from(expirationDateTime.toInstant());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .setIssuedAt(issueDate).setExpiration(expirationDate)
+                .compact();
+    }
+}
