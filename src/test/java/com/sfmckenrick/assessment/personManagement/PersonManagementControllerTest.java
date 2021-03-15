e5ae6d60604944c5440a6102b99cff8c6b93e81f
Scott McKenrick <sbm5967@arl.psu.edu>
2021-03-15 03:27:08 -0400

    Updates unit tests to use basic auth.

--- src/test/java/com/sfmckenrick/assessment/personManagement/PersonManagementControllerTest.java
+++ src/test/java/com/sfmckenrick/assessment/personManagement/PersonManagementControllerTest.java
@@ -2,6 +2,8 @@ package com.sfmckenrick.assessment.personManagement;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sfmckenrick.assessment.authentication.Authority;
+import com.sfmckenrick.assessment.authentication.Role;
 import com.sfmckenrick.assessment.personManagement.exception.PersonNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.security.Principal;
 import java.util.Date;
 
 
@@ -38,6 +44,7 @@ class PersonManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "public", password = "password", roles = Role.USER, authorities = Authority.READ)
     void testGetPersonByIdExists() throws Exception {
         Person expected = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
         service.savePerson(expected);
@@ -52,6 +59,7 @@ class PersonManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "public", password = "password", roles = Role.USER, authorities = Authority.READ)
     void testGetPersonByIdNotExists() throws Exception {
         String result = mockMvc.perform(MockMvcRequestBuilders
                 .get(GET_MAPPING, 1))
@@ -62,6 +70,7 @@ class PersonManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "admin", authorities = Authority.WRITE)
     void testDeletePersonByIdExists() throws Exception {
         Person expected = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
         service.savePerson(expected);
@@ -75,6 +84,7 @@ class PersonManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "admin", authorities = Authority.WRITE)
     void testDeletePersonByIdNotExists() throws Exception {
         String result = mockMvc.perform(MockMvcRequestBuilders
                 .delete(DELETE_MAPPING, 1))
@@ -85,6 +95,7 @@ class PersonManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "admin", authorities = Authority.WRITE)
     void testInsertPerson() throws Exception {
         Person person = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
         String json = new ObjectMapper().writeValueAsString(person);
@@ -107,6 +118,7 @@ class PersonManagementControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "admin", authorities = Authority.WRITE)
     void testInsertPersonMalformed() throws Exception {
         Person person = new Person("John", null, "Doe", new Date(System.currentTimeMillis()));
         String json = new ObjectMapper().writeValueAsString(person).replace("first", "frist");
